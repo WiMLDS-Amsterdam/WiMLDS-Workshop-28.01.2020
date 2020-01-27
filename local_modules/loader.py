2f9fffbe7eab57c88547cedfe49c0f5e7a4869df
Adamantia <triantou.adamantia@gmail.com>
2020-01-27 15:55:24 +0100

    added data and local python modules

---
+++ local_modules/loader.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 12 11:57:06 2020
+
+@author: mhader
+"""
+
+
+import os
+
+def load_data(path):
+
+    """
+
+    Load dataset
+
+    """
+
+    input_file = os.path.join(path)
+
+    with open(input_file, "r") as f:
+
+        data = f.read()
+
+
+
+    return data.split('\n')
